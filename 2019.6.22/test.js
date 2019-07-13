562f352c173e01f4874a14def0376d957a9f1393
ZTaer <ztaerkiller@gmail.com>
2019-07-13 09:05:07 +0800

    第一次在Udemy学习js

---
+++ 2019.6.22/test.js
@@ -0,0 +1,8 @@
+
+var a=1;
+alert( Boolean(a) );
+
+var b = 01;
+alert(typeof(b));
+b = 08;
+alert(typeof(b));
---
+++ 2019.7.13/script.js
@@ -0,0 +1,8 @@
+console.log('hellow word!');
+
+/** js-使用键盘输入字符串 - prompt() **/
+//	0. 默认键入为string类型
+
+var Q = prompt( 'what is his last name?' );
+alert( typeof(Q) ); // 键入值为String类型
+console.log( Q );
