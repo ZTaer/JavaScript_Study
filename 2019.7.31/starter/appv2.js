280669e7571b09359bf8b840954a844f2c442869
ztaer <ztaerkiller@gmail.com>
2019-08-04 23:38:14 +0800

    学习到，js高级概念，原型以及赋值细节，但没有做笔记

--- 2019.7.31/starter/appv2.js
+++ 2019.7.31/starter/appv2.js
@@ -1,108 +1,61 @@
-/*
-GAME RULES:
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+/*************** v2.0 */
+    // 功能1: 当骰子连续为2个6时清空所有分数，并切换下一个玩家
+    // 功能2: 玩家自定义胜利分数
+    // 功能3: 增加一个骰子，只要有一个骰子为1则清空临时分数，并切换下一个玩家
 
-*/
+    // 逻辑分析:
+        // 功能1:( 完成 )
+            // Roll按钮中
+                // 当骰子不等于1时记录，当前筛子数为dice_ed( 默认为0,全局变量，别忘记初始化函数时复原 )
+                // 在次单击刷新后，优先判断当前dice == 6 && dice_ed == 6
+                    // 清空所有分数
+                    // 是: 则切换玩家
+        // 功能2:
+            // setting winner按钮中,在所有按钮之上
+                // 初始化游戏
+                // 初始化参数粉数设定为默认100
+                // 建立全局变量winner = 100 默认值
+                    // 创建按钮单击即可，索引前端表单中的值
+                    // 然后赋值给winner变量
+                    // 索引表单值方法: var name = document.getElementById("name").value;
+        // 功能3:
+            // 创建全局变量dice_2
+            // 初始化也要添加为none
+            // 切换玩家时也要none
+            // Roll按钮:
+                // 筛子二生成且返回前端
+                // 原条件: dice !== 1 变为 dice !== 1 && dice_2 !== 1; 
+                    // 并且记录dice_2_end值
+                    // 临时分数改变相加为 dice + dice_2
+                // 原条件: dice == 6 && dice_ed == 6 变为3种情况 ( ( dice == 6 && dice_ed == 6 ) || ( dice_2 == 6 && dice_2_ed == 6 ) ) || ( dice == 6 && dice_2 == 6 )
+            // Hold按钮:
+                // 取消显示骰子加上dice_2
 
-/************ es5-生成随机数，以及转为整数 
-// 0. 生成0~1随机数( 与乘法配合生成其它随机数 )
-    Math.random(); 
-// 1. 非四舍五入取整数
-    Math.floor(); 
-
-// 例如:
-var ran = 0;
-ran = Math.floor( Math.random() * 6 + 1 ); // 生成1~6的随机数
-console.log(ran);
-*/
-
-/************ es5-document.querySelector()HTML标签操控 
-
-// 0. 选中标签
-document.querySelector(' #score-0 '); //  以CSS选择器选中即可
-
-// 1. 改变标签内容
-    // a) 改变标签文本内容: <a id="score-0" > 文本内容 <a>
-    document.querySelector(' #score-0 ').textContent = ran ;
-    // b) 在标签中插入HTML代码: <a id="score-0" > <b> $ran </b> </a>
-    document.querySelector('#score-0').innerHTML= '<b>' + ran + '</b>' ;
-
-// 2. 获取标签内容
-    // a) 获取标签中的文本内容( 字符串类型 )
-    var test0 =  document.querySelector('#score-0').textContent;
-    console.log( test0 );
-
-    // b) 获取标签中，HTML标签
-    var test1 = document.querySelector('.player-0-panel').innerHTML;
-    console.log( test1 );
-
-// 3. 改变标签中CSS样式
-    // a) 其实核心document.querySelector('CSS选择器').style.css属性 = 'css样式参数';
-    document.querySelector('.dice').style.display = 'none';
-
-// 4. 改变标签属性 - 改变图片实例
-     // a) 选中的标签.属性 = '修改值';
-    var choose_img = 3;
-    var dice_img = document.querySelector('.dice');
-    dice_img.src = 'dice-' + choose_img + '.png'; // 核心
-
-
-*/
-
-/************* es5-.addEventListener()监听事件，并执行函数 
-// 监听事件参数参考网站: https://developer.mozilla.org/zh-CN/docs/Web/Events/
-
-    // 0. 正常调用函数 -  监听单机事件,执行test0()函数
-    function test0(){
-        var dice = Math.floor( Math.random() * 6 + 1 ); // 随机数 
-        document.getElementById('score-0').textContent = dice; // 抓取ID标签
-    }
-    var btn_roll = document.querySelector('.btn-roll').addEventListener('click',test0);
-
-    // 1. 调用匿名函数( 匿名函数就是没有变量名称的函数 ) - 监听单击时间，并执行函数
-        // a) .addEventListener('监听事件', function(){ 函数内容 }  );
-    var btn_roll = document.querySelector('.btn-roll').addEventListener('click', function(){ 
-        var dice = Math.floor( Math.random() * 6 + 1 ); // 随机数 
-        document.getElementById('score-0').textContent = dice;
-     } );
-*/
-
-/*************** es5-.getElementById('test')抓取ID 
-     // 0. 抓取ID, 抓取速度比.querySelector()更快，但是功能单一不方便
-     document.getElementById('test');
-*/
-
-/**************** es5-.classList标签多类名操控，删除，添加，切换HTML标签类名 
-     // 删除指定类名,多个类名用逗号隔开
-     document.querySelector('.player-0-panel').classList.remove('active','test','test1');
-
-     // 添加类名,多个类名同样用逗号隔开
-     document.querySelector( '.player-0-panel' ).classList.add('active','name');
-
-     // 切换类名，所为切换: 标签含有类名时删除,无类名时添加
-     document.querySelector('.player-0-panel').classList.toggle('active');
-*/
 
 
 // 玩家得分，临时得分，玩家选择，骰子
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 var gameing; 
+var dice,dice_ed, dice_2,dice_2_ed = 0;
+var winner = 100;
 
 // 初始化函数init()
 function init(){
     scores = [0,0];
     roundScore = 0;
     activePlayer = 0;
+    dice_ed = 0;
+    winner = 100;
     gameing = true; // 控制游戏开始结束，以限制游戏中按钮操作，及游戏进度    
 
     // 初始化游戏界面
     dice = Math.floor( Math.random() * 6 + 1 ); // 随机数 
     document.querySelector('.dice').style.display = 'none'; // 取消dice显示
+    dice_2 = Math.floor( Math.random() * 6 + 1 ); // 随机数 
+    document.querySelector('.dice_2').style.display = 'none'; // 取消dice显示
+
+
 
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
@@ -119,9 +72,6 @@ function init(){
     document.querySelector('.player-0-panel').classList.remove('active');
     document.querySelector('.player-1-panel').classList.remove('active');
     
-    document.querySelector('.dice').style.display = 'none'; // 取消dice显示
-
-
 }
 
 init();
@@ -137,8 +87,19 @@ function next_player(){
     document.querySelector('#current-0').textContent = '0';
     document.querySelector('#current-1').textContent = '0';
     document.querySelector('.dice').style.display = 'none'; // 取消dice显示
+    document.querySelector('.dice_2').style.display = 'none'; // 取消dice显示
     roundScore = 0;
 }
+
+// 设定胜利玩家分数
+document.querySelector('.winner_setting').addEventListener('click',function(){
+    init();
+    var score = document.getElementById('winner_scores').value;
+    score ? winner = score : winner = 100; 
+    console.log( winner );
+
+});
+
 
 // 单击"Roll Dice"按钮骰子进行变化
 document.querySelector('.btn-roll').addEventListener('click',function(){
@@ -149,6 +110,11 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
         var diceDom = document.querySelector('.dice'); // 抓取图片标签
         diceDom.style.display = 'block';
         diceDom.src = 'dice-' + dice + '.png'; //  更改src属性
+
+        var dice_2 = Math.floor( Math.random() * 6 + 1 ); // 确保每一次单击骰子就将刷新 
+        var diceDom = document.querySelector('.dice_2'); // 抓取图片标签
+        diceDom.style.display = 'block';
+        diceDom.src = 'dice-' + dice_2 + '.png'; //  更改src属性
 
         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('active');
@@ -166,10 +132,16 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
                     // 骰子取消显示
                     // 临时分数
         
-        console.log(dice);
-        if( dice !== 1 ){
-            roundScore += dice;
+        if( ( ( dice_ed == 6 && dice == 6 ) || ( dice_2_ed == 6 && dice_2 == 6 ) ) || ( dice == 6 && dice_2 == 6 ) ){
+            console.log(dice,dice_ed);
+            scores[activePlayer] = 0;
+            next_player();
+        }
+        else if( dice !== 1 && dice_2 !== 1 ){
+            roundScore += dice + dice_2;
             document.querySelector('#current-' + activePlayer ).textContent = roundScore;
+            dice_ed = dice;
+            dice_2_ed = dice_2;
         }
         else{
             // 切换玩家
@@ -197,7 +169,7 @@ document.querySelector('.btn-hold').addEventListener('click',function(){
         scores[ activePlayer ] += roundScore;
         document.getElementById('score-' + activePlayer ).textContent = scores[activePlayer] ;
         
-        if( scores[activePlayer] >= 100 ){
+        if( scores[activePlayer] >= winner ){
 
             // 更换标语为Winner，且引用css样式加红标语
             gameing = false; // 标记游戏结束,只有btn-new按钮可用
@@ -206,6 +178,7 @@ document.querySelector('.btn-hold').addEventListener('click',function(){
 
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             document.querySelector('.dice').style.display = 'none'; // 取消dice显示
+            document.querySelector('.dice_2').style.display = 'none'; // 取消dice显示
 
         }
         else{
---
+++ 2019.8.4/starter/script.js
@@ -0,0 +1,129 @@
+// 继承的目的，以及使用概念
+    // 0. 目的是减少重复代码
+    // 1. 使用方法
+        // a) 创建一个蓝图Object对象，命名开头大写，易于区分蓝图对象于普通对象
+        // b) 蓝图的索引用调用，让新建的对象继承蓝图中的属性
+
+
+// 让对象拥有继承函数/属性成为可能的prototype( js的基本继承思路 )
+    // 原因:
+        // 0. 每一个建立的Object对象都拥有prototype属性,所以我们才能在对象中调用一些函数和方法
+        // 1. 同理如果我们自定义的函数也加入到prototype中，在对象中同样可以调用我们自定义的函数
+        // 2. 对象在调用函数时，首先会在自身中搜索函数，如果没用则会去prototype中查找函数并调用，如果prototype中也没用则返回undefine
+    // 用于:
+        // 0. 常用于，与蓝图配合增加函数
+        // 1. 当然也可以增加属性，但是并不常用
+
+
+/********** 蓝图 - 对象继承使用方法,以及prototype使用方法 
+function a(){
+
+}
+
+// 创建蓝图,命名开头大写,易于于函数区分
+var Parsen = function( name, age, address ){
+    this.name = name;
+    this.age = age;
+    this.address = address;
+}
+
+// 使用蓝图创建对象
+    // 原理:
+        // a) new 运算符的作用是: 创建一个空对象
+        // b) 而蓝图为函数，加入this的变量被new操作为对象属性.
+var zhao = new Parsen('zhao','21','大学嘉园');
+var teng = new Parsen('teng','21','赵楼');
+
+console.log( zhao,teng );
+
+// 使用prototype给蓝图对象添加自定义函数
+    // 可用于继承的原因:
+        // 0. prototype在每一个对象或者函数默认拥有的属性( 核心 )
+        // 1. prototype中包含官方自带的函数，所以我们才能在对象中使用
+        // 2. 同理如果将我们创建的自定义函数加入prototype属性中，那么也就可以在对象中调用我们的自定义函数
+    // 常用于:
+        // 0. 给对象增加函数,如蓝图
+        // 1. 也可以添加属性，但是并不常用 
+Parsen.prototype.cale = function(){
+    console.log( 100 - this.age );
+}
+
+zhao.cale(); // 调用函数
+teng.cale();
+*/
+
+/********** 原型链细节补充，以及证明js皆对象  
+    // 0. 在chrome中可以清楚的看到，js万物皆对象，除“ int,float,null,undefine,true/false ”数据类型为源语
+    // 1. 他们都有原型链 __proto__ 目的是方便继承，调用不同的函数或者方法。
+    // 2. 如创建的对象与蓝图中的原型链:
+        // a) zhao.__proto__ === Parsen.prototype // true 成立
+    // 3. 因为有__proto__(原型链)的存在，才可实现不同数据间对函数的灵活调用，且他默认包含官方的默认函数在__proto__中的__proto__可找到，利用谷歌控制器
+
+console.log( zhao.__proto__ === Parsen.prototype ); // true
+
+*/
+
+/********** 在chrome控制器调试小技巧-查看原型链对象构成 
+
+// .hasOwnProperty()可判断对象中是否有指定属性
+zhao.hasOwnProperty( name ); 
+
+// 判断对象蓝图归属
+zhao instanceof Parsen // true
+
+// 查看数据的对象构成细节，如数组的对象细节
+sss = [1,2];
+console.info(sss);
+// 如下图:
+*/
+
+
+/********** 使用Object.creat()打造继承-模板对象蓝图 
+// 前面我们演示了，用函数来缔造蓝图，这次用对象来缔造
+// 使用Object.creat()，缔造对象模板蓝图
+    // 缺点: 比起使用函数来缔造对象蓝图，Object.creat其实只搞些普通的属性确实很麻烦
+    // 优点: 对于函数类型的属性，能够更加容易引用且可读性更高
+    // 推荐使用方法2
+
+// 方法1( 其实这个方法比较灵活 ):
+    //创建蓝图
+    var personProto = {
+        cale: function(){
+            console.log( 2019 - age );
+        }
+    }
+    
+    // 继承蓝图
+    var zhao = Object.create( personProto );
+    zhao.name = 'zhao'; // 继承蓝图后在增加属性
+    zhao.age = '21';
+
+// 方法2( 这个写法是按照js底层的工作方式来写的value代表属性的内容 ):
+    var teng = Object.create( personProto, {
+        // 这里涉及的js的底层工作方式，在书中有提到,本人已解
+        name: { value: 'teng' },
+        age: { value: '21' }
+
+    } );
+
+*/
+
+/********** 关于js的赋值 */
+// 0. 普通变量间的赋值
+    // a) 不同与C，在JS中普通变量间的赋值是真正的赋值
+    // b) 指定的不是内存的同一个位置，但内容相同
+var a = 20,b;
+b = a;
+a = 18;
+console.log(a,b); // 18,20
+
+// 1. 对象之间的赋值
+    // a) 二者之间并非真正的复制，而是指向同一个内存位置
+var test1 = {
+    a : 'abc',
+    b : '2000'
+}
+var test2 = test1;
+test1.a = '233';
+
+console.log( test1.a,test2.a ); // 233,233
