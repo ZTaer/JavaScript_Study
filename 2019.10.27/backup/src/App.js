b5c128a2c198251a751855cdc9ca2a85a72c9c73
ztaer <ztaerkiller@gmail.com>
2019-11-10 00:19:37 +0800

    完成github静态页部署，以及怪兽项目笔记

---
+++ 2019.10.27/backup/src/App.js
@@ -0,0 +1,197 @@
+// import 可以多类型导入
+import React,{ Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
+
+class App extends Component{
+  constructor(){
+   super();
+    this.state = {
+      monsters: [],
+      searchField: '',
+    }
+  }
+
+  // react-关于componentDidMount()此函数是在，componet中render()完只后执行的内容。( 完成笔记 )
+    // 0. 经常用于请求数据,改变this.state数据,以至于react又要调用render()渲染界面。
+  componentDidMount(){
+
+    // async异步套装请求怪物数据
+    async function getMonsters(){
+      try{
+        const users = await axios('https://jsonplaceholder.typicode.com/users');
+        return users;
+      }catch( error ){
+          console.log( error )
+      }
+    }
+    getMonsters().then( cur => {
+      this.setState( { monsters: cur.data } );
+    } );
+
+  }
+
+  /**
+   * react-class方法传递,class类中可以使用'箭头函数'
+   */
+  // 0. 原因: 在react中如果将class函数传递给其它作用域，普通class函数因无this会报错必须借用this.xxx.bind(this);才能正常传递使用
+  // 1. 作用: class中如果使用箭头函数，直接可以传递使用this.xxx;
+  // 2. 解析: 因为'箭头函数'可以继承父类的所有方法属性.因为他们同属于一个作用域
+  // 3. 实战:
+    /**
+     * import React,{Component} from 'react';
+     * 
+     * class App() extends Component{
+     *    constructor(){
+     *      super();
+     *      this.state = {
+     *        xx: '',     
+     *      };
+     *      
+     *      this.tt.bind( this ); // 普通方法, 有他才能正常传递使用
+     *    }
+     *    
+     *    // 普通方法
+     *    tt(){
+     *      this.setState({ xx: 'xxx' });   
+     *    }
+     *    
+     *    // 箭头函数方法
+     *    bb = ()=>{
+     *      this.setState({ xx: 'xxx' });   
+     *    }
+     * 
+     *    render(
+     *      <DivCard1 onChangeEvent={ this.tt } /> // 普通方法, 需bind()配合
+     *      <DivCard2 onChangeEvent={ this.bb } /> // 箭头函数方法, 直接调用
+     *    );
+     * }
+     */
+
+    // b) 箭头函数class类实战
+
+
+  searchChangeEvent = e => {
+    this.setState( { searchField: e.target.value } );
+  }
+  
+  render(){
+    // map()函数渲染JSX方法使用( 用于循环数组, 只不过在这里是的内容是JSX )( 完成笔记 )
+        // 0. 使用map函数的优势:
+          // a) react非常的聪明, 如果map循环的数据中有变化, 那么他只会渲染发生变化的内容, 不会全部重新渲染
+          // b) 这样的方法大大提高了效率 
+        // 1. JSX中map函数使用注意事项: 
+          // a) JSX中大括号中才能写JS语法: { JS语法 }
+          // b) key属性，非常重要react用于标识不同的标签, 
+            // 0. 有map()的地方一定要有key配合
+            // 1. key一般存储id, 放置于JSX标签属性
+          // c) 如果有cur => { return xxx; }别忘记return, map()才能重组数组;
+        // 2. map()无return写法 
+          // { this.state.monsters.map( cur => <h1 key={ cur.id } > { cur.name } </h1> ) }
+        // 3. map()有return写法, 如下:
+
+    const { monsters, searchField } = this.state; // 对象解构
+    const filterMonsters = monsters.filter( cur => cur.name.toLowerCase().includes( searchField.toLowerCase() ) ); // 过滤数组元素,小写字符串方便比较 
+
+    return(
+          /***this.setState异步与回调函数( 等待笔记 )
+           * 0. this.setState( { 改变值 }, 改变后执行的函数 ) -> this.setState( { xxx: xxx }, ()=>{} );
+           * 1. 因为this.setState( {}, function )为异步运行的，所以要在同setState下的，另写一个function确定修改值的准确性。异步可以保证其它js正常运行，可以直接应用到异步需求的搜索栏
+           * 
+            this.setState( { searchField: e.target.value }, ()=>{console.log(this.state)} )
+           */
+          /***JSX属性onChange={}表单内容发生改变触发函数( 等待笔记 )
+           * 0. onChange = { e => {} }
+           * 1. e: 包含表单标签的属性，常用e.target.value获取表单输入值 
+           * 2. 如: <input type='search' onChange={ e => { 注意: 改变this.state值会重新执行render函数 } } /> 
+           */ 
+      <div className="App">
+        <h1> 大怪物用户 </h1>
+        <SearchBox placeholder='搜索怪物名称' searchChangeEvent={ this.searchChangeEvent } />
+        <CardList monsters={filterMonsters} />
+      </div>
+    );
+  }
+
+}
+
+/* 1. 测试Componet/render( return(JSX) )/onClick/this.setState({})
+class App extends Component {
+  constructor(){
+    // 继承蓝图属性
+    super();
+
+    this.state = {
+      string: '这是一个测试',
+    };
+    this.test = {
+      name: 'ztaer',
+    };
+
+  }
+
+  // render()渲染JSX到前端( 完成笔记 )
+  // 0. 函数本身为Component类中的函数, 经过蓝图继承, 可直接调用蓝图函数.
+  // 1. 使用方法: 
+    // a) 前提条件是在Component类下
+    // b) render(){ return( JSX内容 ) };
+  render(){
+    return(
+
+      // JSX是React模仿HTML的内容( 完成笔记 )
+        // 0. JSX大致于HTML相似，只不过更加方便改变参数在JS中
+        // 1. 注意className == class: 目的是为了区分JS的class
+        // 2. JSX中大括号内才可以写JS语法: 如src={ logo }
+        // 3. JSX对应的css导入: import 'xx.css';注意JSX,className对应html的class名称
+      // JSX-关于onClick={ function }用法( 完成笔记 )
+        // 0. 在JSX中添加此方法后，当用户单击时，将重新执行render()函数，也就是前端界面将重新渲染
+        // 1. 经常使用此方法来改变某个变量参数, 来反应到前端
+        // 如: onClick={ ()=>{ this.setState({ string: 'change this' }) } }
+      // this.setState({ string: 'test' }); == this.state.string = 'test';( 完成笔记 )
+        // 0. 此方法能轻松的改变对象属性参数
+        // 1. 注意: 只能使用在this.state对象中, 其它名称对象不行
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            { this.state.string }
+          </p>
+          <button onClick={ () => { this.setState({ string: '测试onClick函数，以及this.setState({})直接访问对象属性的写法' }) } } >
+            Click Change Text
+          </button>
+        
+        </header>
+      </div>
+    );
+
+  }
+
+}
+*/
+
+/* 0. 原始内容
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          { this.state.string }
+        </p>
+        <button onClick={ () => { this.setState({ string: '测试onClick函数，以及this.setState({})直接访问对象属性的写法' }) } } >
+          Click Change Text
+        </button>
+      
+      </header>
+    </div>
+  );
+}
+*/
+
+
+
+export default App;
---
+++ 2019.10.27/backup/src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render()指定ID标签,渲染JSX内容到前端( 完成笔记 )
+    // 0. <App />: 自定义标签
+    // 1. 使用方法: ReactDOM.render( <自定义标签 />, 渲染位置  );
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// 自定义标签( 完成笔记 )
+    // 0. 因为class开头大写, 自定义标签名称与class名相同
+    // 1. 可以使用自定义标签的情况:
+        // a) 类情况: class Xxx extends Component{ render( return( JSX ) ); }
+        // b) 函数情况: 
+            // 0. const Xxx ()=>( JSX ): 小括号其实具有欺骗,编译器的效果,使编译器直接执行/返回小括号内容
+            // 1. const Xxx ()=>{ return (JSX) }
+            // 2. function Xxx(){ return (JSX) }
+    // 3. <Xxx/>自定义标签转换过程 
+        // a) 自定义标签( JSX -> 自定义标签 -> HTML ): 
+    // 4. 关于JSX名称要开头大写
+
+// 合理的文件结构( 完成笔记 )
+    // 0. webpack最终将会把他们打包一起
+    // 0. 关于JSX文件结构化:
+        // a) 类名.文件夹.jsx
+        // b) 示例:
+            /** components文件夹:
+             *  --card文件夹
+             *  ----card.components.jsx
+             *  ----card.style.css
+             *  
+             *  --search-box文件夹
+             *  ----search.components.jsx
+             *  ----search.style.css 
+             */
+            
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
