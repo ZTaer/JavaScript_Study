bab2ab3ae5f1c5d4b0f060b383efae3b9f845eae
ztaer <ztaerkiller@gmail.com>
2019-12-09 02:16:53 +0800

    明天开始学习高效表单的组件创建，然后在整理笔记

---
+++ 2019.12.3/shopping-react/src/components/header/header.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./header.styles.scss";
+import { Link } from "react-router-dom";
+
+// React-React导入svg文件( 等待笔记 )
+    // 0. import { ReactComponent as Logo } from "../../assets/crown.svg";
+    // 1. 因为文件为jsx文件格式，所以需要借用 ReactComponent 这是规则，
+    // 2. { ReactComponent as Logo }修改名称为logo 
+    // 3. 这样就可以使用自定义标签来代表svg图像<Logo />
+    // 4. SVG的优势: svg图标，为矢量图，并且很小
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+
+const Header = () => {
+    return(
+        <div className="header">
+            <Link className="logo-container" to="/" >
+                <Logo className="logo" />
+            </Link>
+            <div className="options">
+                <Link className="option" to="/shop" >
+                    产品
+                </Link>
+                <Link className="option" to="/shop" >
+                    联系
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
+
---
+++ 2019.12.3/shopping-react/src/pages/shoppage/shoppage.component.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import "./shopage.styles.scss";
+import shopData from "./shop.data";
+
+import CollectionView from "../../components/collection-view/collection-view.component";
+
+class ShopPage extends React.Component  {
+    constructor( props ){
+        super( props );
+        this.state = {
+            collectionShop: shopData,
+        }
+    }
+
+    render(){
+        console.log( shopData );
+        // 对象解构法 - 创建对象属性变量并赋值( 等待笔记 )
+            // 0. const { collectionShop } = this.state; 相当于 const collectionShop = this.state.collectionShop;
+            // 1. 注意属性名称与变量名称一致，以及注意大括号。
+        const { collectionShop } = this.state;
+        return (
+            <div className="shop-page">
+                {
+                    collectionShop.map( ( {id,...otherProps} )=>(
+                       <CollectionView key={id} {...otherProps} /> 
+                    ) )
+                }
+            </div>
+        );
+    }
+
+}
+
+export default ShopPage;
