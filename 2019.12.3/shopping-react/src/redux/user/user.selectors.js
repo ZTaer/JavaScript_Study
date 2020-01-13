5ac9e19b39897514f2e8bf50adc27014c5d46a89
ztaer <ztaerkiller@gmail.com>
2020-01-13 17:30:25 +0800

    为方便react电商项目部署在heroku中，故将react电商项目移出到react-shopping-test, 完成部分heroku使用笔记

--- 2019.12.3/shopping-react/src/redux/user/user.selectors.js
+++ 2019.12.3/shopping-react/src/redux/user/user.selectors.js
@@ -6,3 +6,18 @@ export const selectUserCurrentUser = createSelector(
     [selectUser],
     user => user.currentUser,
 );
+
+export const selectUserImg = createSelector(
+    [selectUserCurrentUser],
+    currentUser => {
+        const defaultImg = 'https://svgshare.com/i/CUz.svg';
+        return currentUser ?
+        ( currentUser.photoURL ? currentUser.photoURL : defaultImg ) :
+        defaultImg
+    },
+);
+
+export const selectUserEmail = createSelector(
+    [selectUserCurrentUser],
+    currentUser => currentUser ? currentUser.email : '请登录!!',
+);
