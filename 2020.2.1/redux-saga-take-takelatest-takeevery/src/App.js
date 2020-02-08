2f6d17dbb389a28490fbfdc81f820ce027fde40a
ztaer <ztaerkiller@gmail.com>
2020-02-08 17:07:29 +0800

    完成redux-saga学习, 准备学习react-hooks

---
+++ 2020.2.1/redux-saga-take-takelatest-takeevery/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SagasExample from './components/sagas-example/sagas-example.component';
+
+import './App.css';
+
+const App = props => {
+  return (
+    <div className='App'>
+      <SagasExample />
+    </div>
+  );
+};
+
+export default App;
