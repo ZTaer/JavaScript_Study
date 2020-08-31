662a85ff932a9cdf3918417562511b98ba996600
ztaer <ztaerkiller@gmail.com>
2020-09-01 00:17:46 +0800

    搞清楚了popper组件，使用方式，以及2020.8.11 - log.txt增加了，常用的vscode快捷键，以供参考

--- 2020.8.11_Material-UI/0-first-use-material-ui/hello-material-ui/src/components/popper-mui-test/popper-mui-test.component.jsx
+++ 2020.8.11_Material-UI/0-first-use-material-ui/hello-material-ui/src/components/popper-mui-test/popper-mui-test.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useRef, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Popper from '@material-ui/core/Popper';
 
@@ -11,23 +11,40 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function SimplePopper() {
+
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
 
-  const handleClick = (event) => {
-      console.log('object :>> ', event.currentTarget);
-    setAnchorEl(anchorEl ? null : event.currentTarget);
+  /**
+   * Popper组件: 隐藏/显示容器组件( 等待笔记 )
+   *    a) 显示Popper条件
+   *        0. open属性：为true
+   *        1. anchorEl属性: 存在dom时
+   *        2. 为什么需要anchorEl属性?: 因为要通过dom决定渲染Popper显示的位置 
+   *        3. 注意: 如果open为true, 而anchorEl的dom不存在, 则报错
+   *    b) 关闭Popper条件
+   *        0. open属性: 为false
+   */
+  const testRef = useRef(null);           // useRef: 方便获取dom
+  const [testOpen, setTestOpen] = useState(false)
+
+  // 函数：单纯的切换open的true/false
+  const testButtonClick = () => {
+    setTestOpen( !testOpen );
   };
-
-  const open = Boolean(anchorEl);
-  const id = open ? 'simple-popper' : undefined;
 
   return (
     <div>
-      <button aria-describedby={id} type="button" onClick={handleClick}>
+      <button 
+        ref={testRef}                     // ref: 配合useRef，用于anchorEl帮助Popper确定渲染的位置
+        type="button" 
+        onClick={testButtonClick} 
+      >
         Toggle Popper
       </button>
-      <Popper id={id} open={open} anchorEl={anchorEl}>
+      <Popper 
+        open={ testOpen }                 // open属性: true/false决定是否显示Popper组件内容 
+        anchorEl={ testRef.current }      // anchorEl: 通过useRef获取的dom，决定Popper渲染位置
+      >
         <div className={classes.paper}>The content of the Popper.</div>
       </Popper>
     </div>
