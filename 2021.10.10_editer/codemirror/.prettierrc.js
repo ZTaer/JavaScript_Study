a5dc530779c03dba7d3b549dc46cfa008ae0fd06
ZTaer <ztaerkiller@gmail.com>
2021-10-10 01:26:48 +0800

    test add

---
+++ 2021.10.10_editer/codemirror/.prettierrc.js
@@ -0,0 +1,12 @@
+/**
+ * 注意: 这里优先级不高, 如出现冲突请在eslint下的'prettier/prettier'配置
+ */
+module.exports = {
+	trailingComma: 'es5',
+	tabWidth: 4,
+	semi: true,
+	singleQuote: true,
+	printWidth: 80,
+	endOfLine: 'auto',
+	useTabs: true,
+};
---
+++ 2021.10.10_editer/monaco/.eslintrc.js
@@ -0,0 +1,73 @@
+require('dotenv').config();
+
+const { NODE_ENV } = process.env;
+
+const config = {
+	root: true,
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		'airbnb-typescript',
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+		'prettier',
+		'prettier/react',
+		'prettier/@typescript-eslint',
+	],
+	overrides: [
+		{
+			files: ['*.ts', '*.tsx', '*/**.ts'],
+			parserOptions: {
+				project: ['./tsconfig.json'],
+			},
+		},
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+		project: ['./tsconfig.eslint.json'],
+	},
+	plugins: ['react', '@typescript-eslint', 'prettier'],
+	rules: {
+		'no-console': NODE_ENV === 'development' ? 'off' : 'error',
+		'import/newline-after-import': 'error',
+		quotes: ['error', 'single'],
+		'prettier/prettier': [
+			'warn',
+			{
+				endOfLine: 'auto',
+				semi: true,
+			},
+		],
+		semi: 'off',
+		'@typescript-eslint/semi': 'off',
+		'arrow-body-style': ['error', 'as-needed'],
+		'@typescript-eslint/comma-dangle': [
+			'error',
+			{
+				arrays: 'always-multiline',
+				exports: 'always-multiline',
+				functions: 'never',
+				imports: 'always-multiline',
+				objects: 'always-multiline',
+			},
+		],
+		'@typescript-eslint/no-var-requires': ['off'],
+		'import/order': 'error',
+		'import/first': 'error',
+		'import/no-unresolved': 'off',
+		indent: 'off',
+		'@typescript-eslint/indent': 'off',
+		'@typescript-eslint/no-unused-vars': ['error'],
+	},
+};
+
+module.exports = config;
