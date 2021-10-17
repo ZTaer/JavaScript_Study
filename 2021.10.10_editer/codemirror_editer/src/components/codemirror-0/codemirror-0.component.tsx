0b6ec3cb055c55fbf623448b77a2666e0cb9f0d6
ZTaer <ztaerkiller@gmail.com>
2021-10-17 20:55:11 +0800

    codemirror基本解析

--- 2021.10.10_editer/codemirror_editer/src/components/codemirror-0/codemirror-0.component.tsx
+++ 2021.10.10_editer/codemirror_editer/src/components/codemirror-0/codemirror-0.component.tsx
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
-import { Controlled as Codemirror } from 'react-codemirror2';
+import { UnControlled as Codemirror } from 'react-codemirror2';
 import { MainProps } from './index.model';
+
+/**
+ * Codemirror基本使用解析( 等待笔记 )
+ */
 
 // 基本样式
 import 'codemirror/lib/codemirror.css';
 
 // 渲染语言模式
+//		a) 注意: 先查看'codemirror/mode/meta.js'确定语言所在文件类型位置
+//		b) java: 'text/x-java' --> import 'codemirror/mode/click/click';
+//		c) json: 'application/ld+json' --> import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/javascript/javascript';
 
 // 皮肤
@@ -30,9 +37,9 @@ const Codemirror0 = (props: MainProps) => {
 	/**
 	 * 交互逻辑区
 	 */
-	const handleUiChangeValue = (data, _update) => {
+	const handleUiChangeValue = (_editer, _data, inputValue) => {
 		try {
-			setValueData(data);
+			setValueData(inputValue);
 		} catch {
 			console.warn('handleUiChangeValue error');
 		}
@@ -45,17 +52,17 @@ const Codemirror0 = (props: MainProps) => {
 			<Codemirror
 				className="codemirror-0-core"
 				value={defaultData}
-				onChange={(...e) => console.log('e :>> ', e)}
-				onBeforeChange={(...e) => console.log('e-before :>> ', e)}
+				onChange={handleUiChangeValue}
 				autoScroll={false}
 				autoCursor={false}
 				options={{
-					mode: 'application/ld+json',
-					lineNumbers: true,
-					theme: 'custom-theme-monaco-light',
-					lineWrapping: true,
-					foldGutter: true,
+					mode: 'application/ld+json', // 语言模式
+					theme: 'custom-theme-monaco-light', // 皮肤
+					lineNumbers: true, // 左边代码行数
+					lineWrapping: true, // 自动换行
+					foldGutter: true, // 折叠
 					gutters: [
+						// 折叠
 						'CodeMirror-linenumbers',
 						'CodeMirror-foldgutter',
 					],
